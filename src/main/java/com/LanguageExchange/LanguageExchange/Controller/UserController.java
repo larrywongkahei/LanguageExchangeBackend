b3d8afddfc53026a4f593727f1751df8461821a5
Maryhei <makemak123@gmail.com>
2023-06-15 21:22:58 +0100

    Added code

--- src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
+++ src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
@@ -1,4 +1,13 @@
 package com.LanguageExchange.LanguageExchange.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/")
+    public String index(){
+        return "hello";
+    }
 }
