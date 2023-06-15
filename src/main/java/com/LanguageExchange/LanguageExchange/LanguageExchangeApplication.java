82ec604eb0edb56017c2b7df3d46835499e664f5
Maryhei <makemak123@gmail.com>
2023-06-15 20:32:15 +0100

    First initial

---
+++ src/main/java/com/LanguageExchange/LanguageExchange/LanguageExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.LanguageExchange.LanguageExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LanguageExchange/LanguageExchange/LanguageExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LanguageExchange.LanguageExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
