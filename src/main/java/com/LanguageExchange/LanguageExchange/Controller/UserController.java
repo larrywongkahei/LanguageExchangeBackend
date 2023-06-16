8671eccff2b5b1fef5b07c28f605c32bbde5b763
Maryhei <makemak123@gmail.com>
2023-06-16 18:52:46 +0100

    Created a application.yaml instead of putting configuration in application properties and tested the endpoint

--- src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
+++ src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.LanguageExchange.LanguageExchange.Controller;
 
 import com.LanguageExchange.LanguageExchange.Model.LanguageLevel;
 import com.LanguageExchange.LanguageExchange.Model.User;
+import com.LanguageExchange.LanguageExchange.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("")
-    public ResponseEntity<User> getAll(){
-        return new ResponseEntity<>(newuser, HttpStatus.OK);
+    public ResponseEntity<List<User>> getAll(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/LanguageExchange/LanguageExchange/LanguageExchangeApplication.java
+++ src/main/java/com/LanguageExchange/LanguageExchange/LanguageExchangeApplication.java
@@ -1,13 +1,29 @@
 package com.LanguageExchange.LanguageExchange;
 
+import com.LanguageExchange.LanguageExchange.Model.LanguageLevel;
+import com.LanguageExchange.LanguageExchange.Model.User;
+import com.LanguageExchange.LanguageExchange.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LanguageExchangeApplication {
+import java.util.HashMap;
 
+@SpringBootApplication
+public class LanguageExchangeApplication implements ApplicationRunner {
+
+	@Autowired
+	UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(LanguageExchangeApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		HashMap hashmap = new HashMap<String, LanguageLevel>();
+		User user = new User("fasdf", "fasefa", "fasef", "afesf", "faesfa", "fasef", "faefs", hashmap);
+		userRepository.save(user);
+	}
 }
