e83d2ed4957a32ad237cf8edd26fd24e7e84c10f
Maryhei <makemak123@gmail.com>
2023-06-16 18:23:20 +0100

    Added getter and setter, testing

--- src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
+++ src/main/java/com/LanguageExchange/LanguageExchange/Controller/UserController.java
@@ -1,16 +1,28 @@
 package com.LanguageExchange.LanguageExchange.Controller;
 
+import com.LanguageExchange.LanguageExchange.Model.LanguageLevel;
+import com.LanguageExchange.LanguageExchange.Model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public ResponseEntity<User> index(){
-        return "hello";
+        HashMap themap = new HashMap<String, LanguageLevel>();
+        themap.put("Larry", LanguageLevel.BEGINNER);
+        User newuser = new User("fasef", "fasef", "fasef", "fasef", "fasef", "fasef", "fasef", themap);
+        System.out.println(newuser.);
+        return new ResponseEntity<>(newuser, HttpStatus.OK);
+
     }
 }
--- src/main/java/com/LanguageExchange/LanguageExchange/Model/User.java
+++ src/main/java/com/LanguageExchange/LanguageExchange/Model/User.java
@@ -1,8 +1,12 @@
 package com.LanguageExchange.LanguageExchange.Model;
 
-import java.util.Date;
-import java.util.Dictionary;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Base64;
+import java.util.Dictionary;
+import java.util.HashMap;
+
+@Document("User")
 public class User {
 
     private String email;
@@ -18,6 +22,80 @@ public class User {
 
     private String motherTongue;
 
-    private Dictionary<String, LanguageLevel> learningLanguage;
+    private HashMap<String, LanguageLevel> learningLanguage;
 
+    public User(String email, String firstName, String lastName, String dataOfBirth, String gender, String countryOfResidence, String motherTongue, HashMap<String, LanguageLevel> learningLanguage) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dataOfBirth = dataOfBirth;
+        this.gender = gender;
+        this.countryOfResidence = countryOfResidence;
+        this.motherTongue = motherTongue;
+        this.learningLanguage = learningLanguage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(String dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountryOfResidence() {
+        return countryOfResidence;
+    }
+
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public String getMotherTongue() {
+        return motherTongue;
+    }
+
+    public void setMotherTongue(String motherTongue) {
+        this.motherTongue = motherTongue;
+    }
+
+    public HashMap<String, LanguageLevel> getLearningLanguage() {
+        return learningLanguage;
+    }
+
+    public void setLearningLanguage(HashMap<String, LanguageLevel> learningLanguage) {
+        this.learningLanguage = learningLanguage;
+    }
 }
